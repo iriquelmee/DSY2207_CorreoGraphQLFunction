2eca4926027c95511a94e1585d9b1eccdfe86dd1
iriquelmee <i.riquelmee@duocuc.cl>
2025-04-12 00:04:59 -0400

    Fix add deploy azure function mapeo directorio de wallet oracle en cloud

--- src/main/java/com/grupo8/config/OracleConnectionUtil.java
+++ src/main/java/com/grupo8/config/OracleConnectionUtil.java
@@ -6,8 +6,8 @@ import java.sql.DriverManager;
 public class OracleConnectionUtil {
 
     public static Connection getConnection() throws Exception {
-        String walletPath = "C:/Wallet_PB06O46XMPFJU1B9";
-        //String walletPath = "/home/site/wwwroot/wallet";
+        //String walletPath = "C:/Wallet_PB06O46XMPFJU1B9";
+        String walletPath = "/home/site/wwwroot/wallet";
         System.setProperty("oracle.net.tns_admin", walletPath);
         System.setProperty("oracle.net.ssl_server_dn_match", "true");
 
